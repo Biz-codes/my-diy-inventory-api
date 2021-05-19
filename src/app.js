8b8ba3ff02d1f5177eb4d7cea13b609b7a72dd01
Biz Hight <elizabeth.biz.hight@gmail.com>
2021-05-19 16:00:07 -0400

    commented out unneeded code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const usersRouter = require('./users/users-router')
-const suppliesRouter = require('./supplies/supplies-router')
+// const suppliesRouter = require('./supplies/supplies-router')
 
 const app = express()
 
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(helmet())
 
 app.use('/api/users', usersRouter)
-app.use('/api/supplies', suppliesRouter)
+// app.use('/api/supplies', suppliesRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
