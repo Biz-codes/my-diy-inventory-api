4548d564c0affd963ee2adb207123a95f45544a4
Biz Hight <elizabeth.biz.hight@gmail.com>
2021-05-20 16:21:17 -0400

    POST and DELETE projects

--- src/projects/projects-router.js
+++ src/projects/projects-router.js
@@ -1,12 +1,12 @@
-const path = require('path')
-const express = require('express')
-const xss = require('xss')
-const ProjectsService = require('./projects-service')
+const path = require("path");
+const express = require("express");
+const xss = require("xss");
+const ProjectsService = require("./projects-service");
 
-const projectsRouter = express.Router()
-const jsonParser = express.json()
+const projectsRouter = express.Router();
+const jsonParser = express.json();
 
-const serializeProject = project => ({
+const serializeProject = (project) => ({
   id: project.id,
   user_id: project.user_id,
   project_name: xss(project.project_name),
@@ -14,18 +14,80 @@ const serializeProject = project => ({
   tools_needed: xss(project.tools_needed),
   instructions: xss(project.instructions),
   delivery_date: project.delivery_date,
-  done: project.done
-})
+  done: project.done,
+});
 
 projectsRouter
-  .route('/')
+  .route("/")
   .get((req, res, next) => {
-    const knexInstance = req.app.get('db')
+    const knexInstance = req.app.get("db");
     ProjectsService.getAllProjects(knexInstance)
-      .then(projects => {
-        res.json(projects.map(serializeProject))
+      .then((projects) => {
+        res.json(projects.map(serializeProject));
       })
-      .catch(next)
+      .catch(next);
   })
 
-  module.exports = projectsRouter
+  .post(jsonParser, (req, res, next) => {
+    const {
+      user_id,
+      project_name,
+      supplies_needed,
+      tools_needed,
+      instructions,
+      delivery_date,
+      done,
+    } = req.body;
+    const newSupply = {
+      user_id,
+      project_name,
+      supplies_needed,
+      tools_needed,
+      instructions,
+      delivery_date,
+      done,
+    };
+
+    for (const [key, value] of Object.entries(newProject))
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` },
+        });
+
+    ProjectsService.insertProject(req.app.get("db"), newProject)
+      .then((project) => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${project.id}`))
+          .json(serializeProject(project));
+      })
+      .catch(next);
+  });
+
+projectsRouter
+  .route("/:project_id")
+  .all((req, res, next) => {
+    ProjectsService.getById(req.app.get("db"), req.params.project_id)
+      .then((project) => {
+        if (!project) {
+          return res.status(404).json({
+            error: { message: `Project doesn't exist` },
+          });
+        }
+        res.project = project;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.json(serializeProject(res.project));
+  })
+  .delete((req, res, next) => {
+    ProjectsService.deleteProject(req.app.get("db"), req.params.project_id)
+      .then((numRowsAffected) => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
+
+module.exports = projectsRouter;
--- src/projects/projects-service.js
+++ src/projects/projects-service.js
@@ -3,6 +3,30 @@ const ProjectsService = {
       return knex.select('*').from('projects')
     },
 
+    insertProject(knex, newProject) {
+        return knex
+          .insert(newProject)
+          .into('projects')
+          .returning('*')
+          .then(rows => {
+            return rows[0]
+          })
+      },
+  
+      getById(knex, id) {
+        return knex
+          .from('projects')
+          .select('*')
+          .where('id', id)
+          .first()
+      },
+    
+      deleteProject(knex, id) {
+        return knex('projects')
+          .where({ id })
+          .delete()
+      },
+
 }
   
 module.exports = ProjectsService
