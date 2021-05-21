2f78c40eaeccc67dccb8bfc3be8eb5f23c001731
Biz Hight <elizabeth.biz.hight@gmail.com>
2021-05-21 01:00:57 -0400

    fixed typo

--- seeds/seed.projects.sql
+++ seeds/seed.projects.sql
@@ -1,4 +1,4 @@
 INSERT INTO projects (user_id, project_name, supplies_needed, tools_needed, instructions, delivery_date, done)
     VALUES
-        ('Raised Bed', 2, 'chicken wire, screws, staples, wood', 'drill, staple gun', '...', '2019-07-04', true),
-        ('Stormy Pants', 2, 'blue thread, jersey knit cotton, pattern', 'marking pen, pins, scissors, sewing machine, sewing machine needle', '...', '2021-09-30', false);
+        (2, 'Raised Bed', 'chicken wire, screws, staples, wood', 'drill, staple gun', '...', '2019-07-04', true),
+        (2, 'Stormy Pants', 'blue thread, jersey knit cotton, pattern', 'marking pen, pins, scissors, sewing machine, sewing machine needle', '...', '2021-09-30', false);
