eeacf3067b7e85bc38851bc5c45ea5d15c95d2aa
Biz Hight <elizabeth.biz.hight@gmail.com>
2021-05-26 13:07:40 -0400

    changed Database URL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgres://chyblsxitcryks:e268c742d3d592617bbaf7e0a64476d5fe67bd08cf4215a91405df4afafc5679@ec2-107-20-153-39.compute-1.amazonaws.com:5432/d7qk8ot5u6enpe',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://biz@localhost/my-diy-inventory',
     JWT_SECRET: process.env.JWT_SECRET || 'Hazel',
     // API_TOKEN: process.env.API_TOKEN || '',
     CLIENT_ORIGIN: '*',
